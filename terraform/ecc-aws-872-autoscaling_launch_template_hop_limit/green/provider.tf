41b19112ab0fbfec144211e49baec0af17376c76
Anatolii Voloshyn <aavolosh@gmail.com>
2024-03-27 18:31:12 +0200

    new: add policy ecc-aws-872-autoscaling_launch_template_hop_limit

---
+++ terraform/ecc-aws-872-autoscaling_launch_template_hop_limit/green/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5"
+    }
+  }
+}
+
+provider "aws" {
+  profile = var.profile
+  region  = var.default-region
+  default_tags {
+    tags = {
+      CustodianRule    = "ecc-aws-872-autoscaling_launch_template_hop_limit"
+      ComplianceStatus = "Green"
+    }
+  }
+}
