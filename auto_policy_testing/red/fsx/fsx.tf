e9926c95903ad6c121a3fe0c5c0c77f98b09f429
Anna Shcherbak <Anna_Shcherbak@epam.com>
2024-05-21 11:29:46 +0300

    skip: fix fsx autotest

--- auto_policy_testing/red/fsx/fsx.tf
+++ auto_policy_testing/red/fsx/fsx.tf
@@ -7,10 +7,6 @@ resource "aws_fsx_lustre_file_system" "this" {
   drive_cache_type                = "NONE"
   per_unit_storage_throughput     = 12
   provider                        = aws.provider2
-
-  tags = {
-    Name = "${module.naming.resource_prefix.fsx}-lustre"
-  }
 }
 
 resource "aws_fsx_backup" "this" {
@@ -38,6 +34,7 @@ resource "aws_fsx_windows_file_system" "this" {
   subnet_ids          = [data.terraform_remote_state.common.outputs.vpc_subnet_1_id]
   throughput_capacity = 8
   skip_final_backup   = true
+  automatic_backup_retention_days = 0
   deployment_type     = "SINGLE_AZ_2"
 
   tags = {
@@ -62,8 +59,8 @@ resource "aws_fsx_openzfs_file_system" "this" {
 # resource creation may take about 30 minutes
 resource "aws_fsx_ontap_file_system" "this" {
   storage_capacity    = 1024
-  subnet_ids          = [data.terraform_remote_state.common.outputs.vpc_subnet_1_id, data.terraform_remote_state.common.outputs.vpc_subnet_3_id]
-  deployment_type     = "MULTI_AZ_1"
+  subnet_ids          = [data.terraform_remote_state.common.outputs.vpc_subnet_1_id]
+  deployment_type     = "SINGLE_AZ_1"
   storage_type        = "HDD"
   throughput_capacity = 128
   preferred_subnet_id = data.terraform_remote_state.common.outputs.vpc_subnet_1_id
