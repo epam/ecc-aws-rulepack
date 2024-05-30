24fa4b4465d56024ecd3f8ebe024b20c950e0013
Anna Shcherbak <Anna_Shcherbak@epam.com>
2024-05-30 16:49:53 +0300

    skip: update autotest launch-config

--- auto_policy_testing/red/launch/launch.tf
+++ auto_policy_testing/red/launch/launch.tf
@@ -3,23 +3,9 @@ resource "aws_launch_configuration" "this" {
   image_id                    = data.aws_ami.this.id
   instance_type               = "t2.micro"
   associate_public_ip_address = true 
-
-  # metadata_options {
-  #   http_endpoint               = "enabled"
-  #   http_tokens                 = "required"
-  #   http_put_response_hop_limit = "5"
-  # }
-}
-
-resource "aws_launch_configuration" "this2" {
-  name_prefix                 = "${module.naming.resource_prefix.launch_config}-2"
-  image_id                    = data.aws_ami.this.id
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true 
-
   metadata_options {
     http_endpoint               = "enabled"
-    http_tokens                 = "required"
+    http_tokens                 = "optional"
     http_put_response_hop_limit = "5"
   }
 }
--- auto_policy_testing/red/launch/outputs.tf
+++ auto_policy_testing/red/launch/outputs.tf
@@ -1,6 +1,5 @@
 output "launch" {
   value = {
     launch-config = aws_launch_configuration.this.name
-    ecc-aws-520-autoscaling_launch_config_hop_limit = aws_launch_configuration.this2.name
   }
 }
