11b8861dcb3e8ee8b107f3037719f16d2007a581
Anna Shcherbak <Anna_Shcherbak@epam.com>
2024-05-30 15:42:42 +0300

    skip: update autotest for kinesis

--- auto_policy_testing/green/kinesis/outputs.tf
+++ auto_policy_testing/green/kinesis/outputs.tf
@@ -1,6 +1,6 @@
 output "kinesis" {
   value = {
-    kinesis = aws_kinesis_stream.this.arn
+    kinesis       = aws_kinesis_stream.this.arn
     kinesis-video = aws_kinesis_video_stream.this.arn
   }
 }
--- auto_policy_testing/red/kinesis/outputs.tf
+++ auto_policy_testing/red/kinesis/outputs.tf
@@ -1,6 +1,7 @@
 output "kinesis" {
   value = {
-    kinesis = aws_kinesis_stream.this.arn
-    kinesis-video = aws_kinesis_video_stream.this.arn
+    kinesis                                         = aws_kinesis_stream.this1.arn
+    kinesis-video                                   = aws_kinesis_video_stream.this.arn
+    ecc-aws-120-kinesis_server_data_at_rest_has_sse = aws_kinesis_stream.this2.arn
   }
 }
