45701fb8b6bb4a2bbf061d0098d28835102961f8
Anna Shcherbak <Anna_Shcherbak@epam.com>
2024-07-09 16:20:59 +0300

    new: add policy ecc-aws-021-ebs-volume_without_recent_snapshot

--- terraform/ecc-aws-021-ebs-volume_without_recent_snapshot/green/ebs.tf
+++ terraform/ecc-aws-021-ebs-volume_without_recent_snapshot/green/ebs.tf
@@ -3,10 +3,14 @@ resource "aws_ebs_volume" "this" {
   size              = 8
 
   tags = {
-    Name = "021_ebs_volume_Green"
+    Name = "021_ebs_volume_green"
   }
 }
 
 resource "aws_ebs_snapshot" "this" {
   volume_id = aws_ebs_volume.this.id
+
+  tags = {
+    Name = "021_ebs_snapshot_green"
+  }
 }
