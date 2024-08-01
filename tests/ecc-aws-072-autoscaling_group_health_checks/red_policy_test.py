33f34c8ca338284534fed6da664d931232facc1f
Anna Shcherbak <Anna_Shcherbak@epam.com>
2024-08-02 00:02:04 +0300

    p-fix: fix policy 072

--- tests/ecc-aws-072-autoscaling_group_health_checks/red_policy_test.py
+++ tests/ecc-aws-072-autoscaling_group_health_checks/red_policy_test.py
@@ -2,4 +2,6 @@ class PolicyTest(object):
 
     def test_resources(self, base_test, resources):
         base_test.assertEqual(len(resources), 1)
-        base_test.assertNotEqual(resources[0]['HealthCheckType'], 'EKL')
+        
+        base_test.assertEqual(len(resources[0]['LoadBalancerNames']), 1)
+        base_test.assertNotEqual(resources[0]['HealthCheckType'], "ELB")
