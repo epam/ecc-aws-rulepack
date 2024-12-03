580b13a40da3c1f4667d7f6bd9d7bfb3adca794e
Anna Shcherbak <anna_shcherbak@epam.com>
2024-12-03 07:54:03 +0000

    upd: update policy 164
    Fix filters

--- terraform/ecc-aws-164-redshift_clusters_audit_logging_enabled/red1/redshift.tf
+++ terraform/ecc-aws-164-redshift_clusters_audit_logging_enabled/red1/redshift.tf
@@ -5,17 +5,6 @@ resource "aws_redshift_cluster" "this" {
   master_password              = random_password.this.result
   node_type                    = "dc2.large"
   skip_final_snapshot          = true
-  cluster_parameter_group_name = aws_redshift_parameter_group.this.name
-}
-
-resource "aws_redshift_parameter_group" "this" {
-  name   = "parameter-group-164-red"
-  family = "redshift-1.0"
-
-  parameter {
-    name  = "enable_user_activity_logging"
-    value = "false"
-  }
 }
 
 resource "random_password" "this" {
@@ -23,5 +12,7 @@ resource "random_password" "this" {
   special          = true
   numeric          = true
   min_numeric      = 1
+  min_upper        = 1
+  min_lower        = 1
   override_special = "!#$%*()-_=+[]{}:?"
 }
