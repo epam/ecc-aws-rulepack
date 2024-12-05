682f3edf1fe47bc4e955ae2a404ea06a44400033
Anna Shcherbak <anna_shcherbak@epam.com>
2024-12-05 21:50:14 +0000

    upd: update policies 006, 026

--- tests/ecc-aws-006-rds_retention_backup_is_at_least_7_days/red_policy_test.py
+++ tests/ecc-aws-006-rds_retention_backup_is_at_least_7_days/red_policy_test.py
@@ -3,4 +3,6 @@ class PolicyTest(object):
     def test_resources(self, base_test, resources):
         base_test.assertEqual(len(resources), 1)
         base_test.assertLess(resources[0]['BackupRetentionPeriod'], 7)
+        base_test.assertNotEqual(resources[0]['BackupRetentionPeriod'], 0)
+        base_test.assertNotIn("StatusInfos", resources[0])
         
