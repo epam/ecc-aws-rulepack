c5cd4ce71cadb0dabec49d87d924e8cf6e90ee26
Anna Shcherbak <anna_shcherbak@epam.com>
2024-12-06 16:16:59 +0000

    upd: update policy 224

---
+++ terraform/ecc-aws-224-ec2_instance_imdsv2_enabled/green1/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5"
+    }
+  }
+}
+
+provider "aws" {
+  profile = var.profile
+  region  = var.default-region
+  default_tags {
+    tags = {
+      CustodianRule    = "ecc-aws-224-ec2_instance_imdsv2_enabled"
+      ComplianceStatus = "Green"
+    }
+  }
+}
