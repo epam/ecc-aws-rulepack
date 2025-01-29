5e3c7f5d3d25221abbfcd9361bea87cfd57daa02
Anna Shcherbak <anna_shcherbak@epam.com>
2025-01-29 22:54:48 +0000

    new: added policy ecc-aws-873-rds_database_cluster_engine_no_default_ports

---
+++ terraform/ecc-aws-873-rds_database_cluster_engine_no_default_ports/green/rds.tf
@@ -0,0 +1,25 @@
+resource "random_password" "this" {
+  length           = 12
+  lower            = true
+  min_lower        = 1
+  upper            = true
+  min_upper        = 1
+  special          = true
+  min_special      = 1
+  numeric          = true
+  min_numeric      = 1
+  override_special = "!#$%*()-_=+[]{}:?"
+}
+
+resource "aws_rds_cluster" "this" {
+  cluster_identifier        = "cluster-873-green"
+  engine                    = "mysql"
+  db_cluster_instance_class = "db.c6gd.large"
+  storage_type              = "gp3"
+  allocated_storage         = 20
+  master_username           = "root"
+  master_password           = random_password.this.result
+  skip_final_snapshot       = true
+  port                      = 11111
+  engine_lifecycle_support  = "open-source-rds-extended-support-disabled"
+}
