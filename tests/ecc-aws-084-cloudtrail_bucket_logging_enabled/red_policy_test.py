bd94f23f51fcadae848c339ad876d4c8fe24bec2
Anna Shcherbak <Anna_Shcherbak@epam.com>
2025-07-21 22:17:29 +0300

    upd: update policy 084  to be supported by open source Cloud Custodian

--- tests/ecc-aws-084-cloudtrail_bucket_logging_enabled/red_policy_test.py
+++ tests/ecc-aws-084-cloudtrail_bucket_logging_enabled/red_policy_test.py
@@ -2,5 +2,4 @@ class PolicyTest(object):
 
     def test_resources_with_client(self, base_test, resources, local_session):
         base_test.assertEqual(len(resources), 1)
-        s3 = local_session.client("s3").get_bucket_logging(Bucket=resources[0]["S3BucketName"])
-        base_test.assertNotIn("LoggingEnabled", s3)
+        base_test.assertEqual(resources[0]["Bucket"][0]["Logging"], {})
