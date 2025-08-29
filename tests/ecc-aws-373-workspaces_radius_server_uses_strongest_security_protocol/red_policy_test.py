7684fe9c7a1e09ea310efe6c6514113ea10be40c
Anna Shcherbak <Anna_Shcherbak@epam.com>
2025-08-30 00:55:21 +0300

    upd: update policy 373 to be supported by open source Cloud Custodian

--- tests/ecc-aws-373-workspaces_radius_server_uses_strongest_security_protocol/red_policy_test.py
+++ tests/ecc-aws-373-workspaces_radius_server_uses_strongest_security_protocol/red_policy_test.py
@@ -2,7 +2,6 @@ class PolicyTest(object):
 
     def test_resources_with_client(self, base_test, resources, local_session):
         base_test.assertEqual(len(resources), 1)
-        ds = local_session.client("ds").describe_directories(DirectoryIds=["d-9067b0b800"])
+        ds = local_session.client("ds").describe_directories(DirectoryIds=[resources[0]['DirectoryId']])
         base_test.assertNotEqual(ds['DirectoryDescriptions'][0]['RadiusSettings']['AuthenticationProtocol'],'MS-CHAPv2')
 
-        
